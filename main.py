e3155d129a15be40640eed9cc1203b8d0a29b15f
DanaAlwosaibae <142634164+DanaAlwosaibae@users.noreply.github.com>
2023-09-06 19:53:48 +0300

    done

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 # write your code here
+person={ 
+    'name':'senju',
+     'age': 13,
+     'hobbies':['boxing','runnig','drawing']
+     }
+print(person.get('name'))
+print(person.get('age'))
+person['age']='14'
+person['contry']='japan'
+print(person)
+
+person['hobbies'].append('reading')
+
+print(len(person))
+
+def check_hobbies(person):
+    if (len('hobbies'))>=3:
+        print('wow you are amazing')
+        
+check_hobbies(person)
+
+
+
